4046c5943379d3841f5172d96e70afad23dfb385
OleksandrXXX <alexeev.army@gmail.com>
2023-01-13 20:34:01 +0200

    Add divide

--- src/app/Main.java
+++ src/app/Main.java
@@ -13,6 +13,9 @@ public class Main {
         System.out.println(d);
         int e = subtract(a, b);
         System.out.println(e);
+        int f = divide(a, b);
+        System.out.println(f);
+
     }
 
     private static int add(int a, int b) {
@@ -25,5 +28,9 @@ public class Main {
 
     private static int subtract(int a, int b) {
         return a - b;
+    }
+
+    private static int divide(int a, int b) {
+        return a / b;
     }
 }
